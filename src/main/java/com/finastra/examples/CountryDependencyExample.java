9f29bac54062cf5b5ace50f9ae56ad7dcb36a0d9
beni.regev@finastra.com <qe7uwt2v!>
2021-07-19 10:34:19 +0300

    Initial Verion Adding configuration files, User class and CountryDependencyExample class

---
+++ src/main/java/com/finastra/examples/CountryDependencyExample.java
@@ -0,0 +1,16 @@
+package com.finastra.examples;
+
+import com.finastra.model.User;
+import org.example.enums.Country;
+
+public class CountryDependencyExample {
+    public static void main(String[] args) {
+        User user = new User("Benny", "Regev", Country.ISRAEL);
+        System.out.println("-------------------------------------------------");
+        System.out.println("* pring user object instance using method toString(): ");
+        System.out.println(user);
+        System.out.println("-------------------------------------------------");
+        System.out.println("* pring user object instance using method toJson(): ");
+        System.out.println("User: \n" + user.toJson());
+    }
+}
---
+++ src/main/java/com/finastra/model/User.java
@@ -0,0 +1,59 @@
+package com.finastra.model;
+
+import org.example.enums.Country;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private Country country;
+
+    public User(String firstName, String lastName, Country country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "User: {" +
+                "firstName=\"" + firstName + '"' +
+                ", lastName=\"" + lastName + '"' +
+                ", country=" + country +
+                ", country-code=" + country.getCountryCode() +
+                ", country-language='" + country.getLanguage() + "'" +
+
+                '}';
+    }
+
+    public String toJson() {
+        return "{ \n" +
+                "\tfirstName=\"" + firstName + "\", \n" +
+                "\tlastName=\"" + lastName + "\"\n" +
+                "\tcountry: " + country.toJson() + "\n" +
+                '}';
+    }
+}
