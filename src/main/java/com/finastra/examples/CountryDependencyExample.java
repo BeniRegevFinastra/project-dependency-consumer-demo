6c034cca77535b46cc6e866bf21022b6cd9a288c
beni.regev@finastra.com <qe7uwt2v!>
2021-07-19 17:14:56 +0300

    Initial Verion - Fixing syntax errors

--- src/main/java/com/finastra/examples/CountryDependencyExample.java
+++ src/main/java/com/finastra/examples/CountryDependencyExample.java
@@ -7,10 +7,10 @@ public class CountryDependencyExample {
     public static void main(String[] args) {
         User user = new User("Benny", "Regev", Country.ISRAEL);
         System.out.println("-------------------------------------------------");
-        System.out.println("* pring user object instance using method toString(): ");
+        System.out.println("* print user object instance using method toString(): ");
         System.out.println(user);
         System.out.println("-------------------------------------------------");
-        System.out.println("* pring user object instance using method toJson(): ");
+        System.out.println("* print user object instance using method toJson(): ");
         System.out.println("User: \n" + user.toJson());
     }
 }
